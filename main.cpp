314fdbb78d332ba48c2ddd2d44e8b0e544201961
David Hrachový <davidhrachovy@atlas.cz>
2011-06-03 18:46:46 +0200

    add configure.. scaffold

--- main.cpp
+++ main.cpp
@@ -10,18 +10,20 @@
 #include <algorithm>
 #include "stb_image.h"
 
-int next_power(int x) { return pow(2, ceil(log(x)/log(2))); }
+int next_power ( int x ) {
+    return pow ( 2, ceil ( log ( x ) /log ( 2 ) ) );
+}
 
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    Uint32 rmask = 0xff000000;
-    Uint32 gmask = 0x00ff0000;
-    Uint32 bmask = 0x0000ff00;
-    Uint32 amask = 0x000000ff;
+Uint32 rmask = 0xff000000;
+Uint32 gmask = 0x00ff0000;
+Uint32 bmask = 0x0000ff00;
+Uint32 amask = 0x000000ff;
 #else
-    Uint32 rmask = 0x000000ff;
-    Uint32 gmask = 0x0000ff00;
-    Uint32 bmask = 0x00ff0000;
-    Uint32 amask = 0xff000000;
+Uint32 rmask = 0x000000ff;
+Uint32 gmask = 0x0000ff00;
+Uint32 bmask = 0x00ff0000;
+Uint32 amask = 0xff000000;
 #endif
 
 
@@ -35,30 +37,30 @@ Uint32 timeMile = 0;
 float framesToPrint = 0.0;
 GLuint texture;
 
-string getDigits(float fps) {
+string getDigits ( float fps ) {
     int f = fps;
     string digits;
     stringstream str;
     char c;
-    while (f != 0) {
-        str << (f % 10);
-        str.get(c);
+    while ( f != 0 ) {
+        str << ( f % 10 );
+        str.get ( c );
         digits += c;
         f /= 10;
     }
-    reverse(digits.begin(), digits.end());
+    reverse ( digits.begin(), digits.end() );
     return digits;
 }
 
-void render_text(const string text) {
+void render_text ( const string text ) {
 
-    glDisable(GL_DEPTH_TEST);
-    glMatrixMode(GL_PROJECTION);
+    glDisable ( GL_DEPTH_TEST );
+    glMatrixMode ( GL_PROJECTION );
     glPushMatrix();
     glLoadIdentity();
-    gluOrtho2D(0.0, 500, 0.0, 500);
+    gluOrtho2D ( 0.0, 500, 0.0, 500 );
 
-    glMatrixMode(GL_MODELVIEW);
+    glMatrixMode ( GL_MODELVIEW );
     glPushMatrix();
     glLoadIdentity();
 
@@ -68,55 +70,62 @@ void render_text(const string text) {
 
     SDL_PixelFormat *format;
     SDL_Color color={0,255,0};
-    if (i_surface=TTF_RenderUTF8_Blended(font,text.c_str(),color)) {
-	int w = next_power(i_surface->w);
-	int h = next_power(i_surface->h);
-        o_surface = SDL_CreateRGBSurface(SDL_SWSURFACE, w, h, 32,
-                                         rmask, gmask, bmask, amask);
-        SDL_BlitSurface(i_surface, 0, o_surface, 0);
-        glGenTextures(1, &text_texture);
-        glBindTexture( GL_TEXTURE_2D, text_texture );
-        glTexImage2D(GL_TEXTURE_2D, 0, 4, w, h, 0, GL_BGRA, GL_UNSIGNED_BYTE, o_surface->pixels);
+    if ( i_surface=TTF_RenderUTF8_Blended ( font,text.c_str(),color ) ) {
+        int w = next_power ( i_surface->w );
+        int h = next_power ( i_surface->h );
+        o_surface = SDL_CreateRGBSurface ( SDL_SWSURFACE, w, h, 32,
+                                           rmask, gmask, bmask, amask );
+        SDL_BlitSurface ( i_surface, 0, o_surface, 0 );
+        for ( int i = 0; i < w; i++ )
+            for ( int ii = 0; ii < h; ii++ )
+                if ( ( ( Uint32* ) o_surface->pixels ) [i*w+ii] == 0x00000000 )
+                    ( ( Uint32* ) o_surface->pixels ) [i*w+ii] = ( ( Uint32* ) o_surface->pixels ) [i*w+ii] | 0xff000000;
+        glGenTextures ( 1, &text_texture );
+        glBindTexture ( GL_TEXTURE_2D, text_texture );
+        glTexImage2D ( GL_TEXTURE_2D, 0, 4, w, h, 0, GL_BGRA, GL_UNSIGNED_BYTE, o_surface->pixels );
 // when texture area is small, bilinear filter the closest mipmap
-        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-                         GL_LINEAR );
+        glTexParameterf ( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                          GL_LINEAR );
 // when texture area is large, bilinear filter the original
-        glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+        glTexParameterf ( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
         // 	vector<char> digits = getDigits(framesToPrint);
 // 	for(int i = 0; i < digits.size(); i++) {
 // 		glRasterPos2i(i * 10 , 500 - 13);
 // 		glutBitmapCharacter(GLUT_BITMAP_8_BY_13, digits[i]);
 // 	}
-        glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 1.0f);
-        glVertex3f(0.0f,0,-1);
+        glEnable ( GL_BLEND );
+        glBlendFunc ( GL_SRC_ALPHA, GL_ZERO );
+        glBegin ( GL_QUADS );
+        glTexCoord2f ( 0.0f, 1.0f );
+        glVertex3f ( 0.0f,0,-1 );
 
-        glTexCoord2f(0.0f, 0.0f);
-        glVertex3f(0.0f, h, -1);
+        glTexCoord2f ( 0.0f, 0.0f );
+        glVertex3f ( 0.0f, h, -1 );
 
-        glTexCoord2f(1.0f, 0.0f);
-        glVertex3f(w, h, -1);
-        glTexCoord2f(1.0f, 1.0f);
-        glVertex3f(w, 0.0f, -1);
+        glTexCoord2f ( 1.0f, 0.0f );
+        glVertex3f ( w, h, -1 );
+        glTexCoord2f ( 1.0f, 1.0f );
+        glVertex3f ( w, 0.0f, -1 );
         glEnd();
-        glDeleteTextures(1, &text_texture);
-        SDL_FreeSurface(i_surface);
-        SDL_FreeSurface(o_surface);
+        glDisable ( GL_BLEND );
+        glDeleteTextures ( 1, &text_texture );
+        SDL_FreeSurface ( i_surface );
+        SDL_FreeSurface ( o_surface );
         //SDL_FreeSurface(on_surface);
 
     } else
         cerr << "cannot render font";
-    glMatrixMode(GL_MODELVIEW);
+    glMatrixMode ( GL_MODELVIEW );
     glPopMatrix();
 
-    glMatrixMode(GL_PROJECTION);
+    glMatrixMode ( GL_PROJECTION );
     glPopMatrix();
 
 }
 
 void load_font() {
-    font = TTF_OpenFont("Ubuntu-R.ttf", 16);
-    if (!font) {
+    font = TTF_OpenFont ( "Ubuntu-R.ttf", 16 );
+    if ( !font ) {
         cerr << "TTF_OpenFont: \n" << TTF_GetError();
     }
 }
@@ -125,7 +134,7 @@ void loadTexture() {
 //load image
 
     int x,y,n;
-    unsigned char *data = stbi_load("test.png", &x, &y, &n, 0);
+    unsigned char *data = stbi_load ( "test.png", &x, &y, &n, 0 );
     // ... process data if not NULL ...
     // ... x = width, y = height, n = # 8-bit components per pixel ...
     // ... replace '0' with '1'..'4' to force that many components per pixel
@@ -134,25 +143,25 @@ void loadTexture() {
 
 //gen texture
 
-    glBindTexture( GL_TEXTURE_2D, texture );
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, x, y, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glBindTexture ( GL_TEXTURE_2D, texture );
+    glTexImage2D ( GL_TEXTURE_2D, 0, 3, x, y, 0, GL_RGB, GL_UNSIGNED_BYTE, data );
 // when texture area is small, bilinear filter the closest mipmap
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-                     GL_LINEAR );
+    glTexParameterf ( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                      GL_LINEAR );
 // when texture area is large, bilinear filter the original
-    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-    stbi_image_free(data);
+    glTexParameterf ( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+    stbi_image_free ( data );
 }
 
 
 void renderImage() {
-    glDisable(GL_DEPTH_TEST);
-    glMatrixMode(GL_PROJECTION);
+    glDisable ( GL_DEPTH_TEST );
+    glMatrixMode ( GL_PROJECTION );
     glPushMatrix();
     glLoadIdentity();
-    gluOrtho2D(0.0, 500, 0.0, 500);
+    gluOrtho2D ( 0.0, 500, 0.0, 500 );
 
-    glMatrixMode(GL_MODELVIEW);
+    glMatrixMode ( GL_MODELVIEW );
     glPushMatrix();
     glLoadIdentity();
 
@@ -161,68 +170,65 @@ void renderImage() {
 //bind image to texture
 
 //draw quad
-    glColor3f(0.5, 0.6, 0.8);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    glBindTexture( GL_TEXTURE_2D, texture );
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 0.0f);
-    glVertex3f(0.0f,0,-1);
-    glTexCoord2f(0.0f, 1.0f);
-    glVertex3f(0.0f, 255, -1);
-    glTexCoord2f(1.0f, 1.0f);
-    glVertex3f(255.0f, 255, -1);
-    glTexCoord2f(1.0f, 0.0f);
-    glVertex3f(255.0f, 0.0f, -1);
+    glColor3f ( 0.5, 0.6, 0.8 );
+    glPolygonMode ( GL_FRONT_AND_BACK, GL_FILL );
+    glBindTexture ( GL_TEXTURE_2D, texture );
+    glBegin ( GL_QUADS );
+    glTexCoord2f ( 0.0f, 0.0f );
+    glVertex3f ( 0.0f,0,-1 );
+    glTexCoord2f ( 0.0f, 1.0f );
+    glVertex3f ( 0.0f, 255, -1 );
+    glTexCoord2f ( 1.0f, 1.0f );
+    glVertex3f ( 255.0f, 255, -1 );
+    glTexCoord2f ( 1.0f, 0.0f );
+    glVertex3f ( 255.0f, 0.0f, -1 );
 
 
     glEnd();
 
 
 
-    glMatrixMode(GL_MODELVIEW);
+    glMatrixMode ( GL_MODELVIEW );
     glPopMatrix();
 
-    glMatrixMode(GL_PROJECTION);
+    glMatrixMode ( GL_PROJECTION );
     glPopMatrix();
 }
 
 
 
 
-void reshape(int w, int h)
-{
-    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
-    glMatrixMode(GL_PROJECTION);
+void reshape ( int w, int h ) {
+    glViewport ( 0, 0, ( GLsizei ) w, ( GLsizei ) h );
+    glMatrixMode ( GL_PROJECTION );
     glLoadIdentity();
     //(Left, Right, Bottom, Top, Near, Far);
-    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 100.0);
-    glMatrixMode(GL_MODELVIEW);
+    glFrustum ( -1.0, 1.0, -1.0, 1.0, 1.5, 100.0 );
+    glMatrixMode ( GL_MODELVIEW );
 }
 
 
 
 
-static void quit( int code )
-{
+static void quit ( int code ) {
     /*
      * Quit SDL so we can release the fullscreen
      * mode and restore the previous video settings,
      * etc.
      */
-    TTF_CloseFont(font);
+    TTF_CloseFont ( font );
     TTF_Quit();
     SDL_Quit();
-    delete(objData);
-    glDeleteTextures( 1,&texture );
+    delete ( objData );
+    glDeleteTextures ( 1,&texture );
 
     /* Exit program. */
-    exit( code );
+    exit ( code );
 }
 
 
 
-static void handle_key_down( SDL_keysym* keysym )
-{
+static void handle_key_down ( SDL_keysym* keysym ) {
 
     /*
      * We're only interested if 'Esc' has
@@ -234,7 +240,7 @@ static void handle_key_down( SDL_keysym* keysym )
      */
     switch ( keysym->sym ) {
     case SDLK_ESCAPE:
-        quit( 0 );
+        quit ( 0 );
         break;
     default:
         break;
@@ -243,22 +249,21 @@ static void handle_key_down( SDL_keysym* keysym )
 }
 
 
-static void process_events( void )
-{
+static void process_events ( void ) {
     /* Our SDL event placeholder. */
     SDL_Event event;
 
     /* Grab all the events off the queue. */
-    while ( SDL_PollEvent( &event ) ) {
+    while ( SDL_PollEvent ( &event ) ) {
 
         switch ( event.type ) {
         case SDL_KEYDOWN:
             /* Handle key presses. */
-            handle_key_down( &event.key.keysym );
+            handle_key_down ( &event.key.keysym );
             break;
         case SDL_QUIT:
             /* Handle quit requests (like Ctrl-c). */
-            quit( 0 );
+            quit ( 0 );
             break;
         }
 
@@ -267,44 +272,44 @@ static void process_events( void )
 }
 
 
-static void draw_screen( void )
-{
+static void draw_screen ( void ) {
 
     globalTime = SDL_GetTicks();
-    if (globalTime - timeMile > 100) {
-        framesToPrint = frame/(globalTime - timeMile)*1000;
+    if ( globalTime - timeMile > 100 ) {
+        framesToPrint = frame/ ( globalTime - timeMile ) *1000;
         timeMile = globalTime;
-	countp++;
+        countp++;
         frame = 0;
     }
 
 
     /* Clear the color and depth buffers. */
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
     frame++;
 
-    glMatrixMode(GL_PROJECTION);
+    glMatrixMode ( GL_PROJECTION );
     glLoadIdentity();
     //(Left, Right, Bottom, Top, Near, Far);
-    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 100.0);
-    glMatrixMode(GL_MODELVIEW);
+    glFrustum ( -1.0, 1.0, -1.0, 1.0, 1.5, 100.0 );
+    glMatrixMode ( GL_MODELVIEW );
 
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);
+    glClear ( GL_COLOR_BUFFER_BIT );
+    glColor3f ( 1.0, 1.0, 1.0 );
 
 
     glLoadIdentity();
-    gluLookAt(0.0, 0.0, 50.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-    glTranslatef(timeMile/50 - 10,0,0);
+    gluLookAt ( 0.0, 0.0, 50.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 );
+    glTranslatef ( timeMile/50 - 10,0,0 );
 // 	glutWireCube(2.0);
-    glCallList(1);
-    if(countp == 10) {
-	cout << framesToPrint << " " << "FPS" << endl;
-	countp = 0;
+    glCallList ( 1 );
+    if ( countp == 10 ) {
+        cout << framesToPrint << " " << "FPS" << endl;
+        countp = 0;
     }
-    render_text(getDigits(framesToPrint));
-    //renderImage();
+    renderImage();
+    render_text ( getDigits ( framesToPrint ) );
+
     glFlush();
 
     /*
@@ -327,63 +332,78 @@ static void draw_screen( void )
     SDL_GL_SwapBuffers( );
 }
 
+void setup_texturing() {
+    glEnable ( GL_TEXTURE_2D );
 
+}
+void setup_shading() {
+	
+}
+void setup_lighting() {
+	
+}
 
-static void setup_opengl( int width, int height )
-{
+void setup_matrices() {
+	
+}
 
+void setup_opengl ( int width, int height ) {
+	timeMile = 0.0;
+    glEnable ( GL_BLEND );
+	setup_texturing();
+	setup_shading();
+	setup_lighting();
+	setup_matrices();
 
-    glEnable( GL_TEXTURE_2D );
 
 
 // allocate a texture name
-    glGenTextures( 1, &texture );
+    glGenTextures ( 1, &texture );
     objData = new objLoader();
-    objData->load("test.obj");
+    objData->load ( "test.obj" );
     cout << objData->faceCount;
-    timeMile = 0.0;
+
 
     loadTexture();
-    glNewList(1, GL_COMPILE);
-    glBegin(GL_TRIANGLES);
-    for (int i = 0; i < objData->faceCount; i++) {
-        for (int ii = 0; ii < 3; ii++) {
+    glNewList ( 1, GL_COMPILE );
+    glBegin ( GL_TRIANGLES );
+    for ( int i = 0; i < objData->faceCount; i++ ) {
+        for ( int ii = 0; ii < 3; ii++ ) {
             obj_vector *v = objData->vertexList[objData->faceList[i]->vertex_index[ii]];
-            glVertex3f((GLfloat) v->e[0],(GLfloat) v->e[1],(GLfloat) v->e[2]);
+            glVertex3f ( ( GLfloat ) v->e[0], ( GLfloat ) v->e[1], ( GLfloat ) v->e[2] );
         }
     }
-
     glEnd();
     glEndList();
 
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor ( 0.0, 0.0, 0.0, 0.0 );
+    timeMile = 0;
 
-    float ratio = (float) width / (float) height;
+    float ratio = ( float ) width / ( float ) height;
 
     /* Our shading model--Gouraud (smooth). */
-    glShadeModel( GL_SMOOTH );
+    glShadeModel ( GL_SMOOTH );
 
     /* Set the clear color. */
-    glClearColor( 0, 0, 0, 0 );
+    glClearColor ( 0, 0, 0, 0 );
 
 
     /*
      * Change to the projection matrix and set
      * our viewing volume.
      */
-    glMatrixMode( GL_PROJECTION );
+    glMatrixMode ( GL_PROJECTION );
     glLoadIdentity( );
     /*
      * EXERCISE:
      * Replace this with a call to glFrustum.
      */
-    gluPerspective( 60.0, ratio, 1.0, 1024.0 );
+    gluPerspective ( 60.0, ratio, 1.0, 1024.0 );
 }
 
 
 
-int main( int argc, char* argv[] )
-{
+int main ( int argc, char* argv[] ) {
     /* Information about the current video settings. */
     const SDL_VideoInfo* info = NULL;
     /* Dimensions of our window. */
@@ -395,18 +415,18 @@ int main( int argc, char* argv[] )
     int flags = 0;
 
     /* First, initialize SDL's video subsystem. */
-    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+    if ( SDL_Init ( SDL_INIT_VIDEO ) < 0 ) {
         /* Failed, exit. */
-        fprintf( stderr, "Video initialization failed: %s\n",
-                 SDL_GetError( ) );
-        quit( 1 );
+        fprintf ( stderr, "Video initialization failed: %s\n",
+                  SDL_GetError( ) );
+        quit ( 1 );
     }
 
 
-    if (TTF_Init()==-1) {
+    if ( TTF_Init() ==-1 ) {
         cerr << "TTF_Init: \n" << TTF_GetError();
         SDL_Quit( );
-        exit(1);
+        exit ( 1 );
     }
     load_font();
 
@@ -416,9 +436,9 @@ int main( int argc, char* argv[] )
 
     if ( !info ) {
         /* This should probably never happen. */
-        fprintf( stderr, "Video query failed: %s\n",
-                 SDL_GetError( ) );
-        quit( 1 );
+        fprintf ( stderr, "Video query failed: %s\n",
+                  SDL_GetError( ) );
+        quit ( 1 );
     }
 
     /*
@@ -450,11 +470,11 @@ int main( int argc, char* argv[] )
      * not affect the GL attribute state, only
      * the standard 2D blitting setup.
      */
-    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 5 );
-    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 5 );
-    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 5 );
-    SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 16 );
-    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+    SDL_GL_SetAttribute ( SDL_GL_RED_SIZE, 5 );
+    SDL_GL_SetAttribute ( SDL_GL_GREEN_SIZE, 5 );
+    SDL_GL_SetAttribute ( SDL_GL_BLUE_SIZE, 5 );
+    SDL_GL_SetAttribute ( SDL_GL_DEPTH_SIZE, 16 );
+    SDL_GL_SetAttribute ( SDL_GL_DOUBLEBUFFER, 1 );
 
     /*
      * We want to request that SDL provide us
@@ -471,22 +491,22 @@ int main( int argc, char* argv[] )
     /*
      * Set the video mode
      */
-    if ( SDL_SetVideoMode( width, height, bpp, flags ) == 0 ) {
+    if ( SDL_SetVideoMode ( width, height, bpp, flags ) == 0 ) {
         /*
          * This could happen for a variety of reasons,
          * including DISPLAY not being set, the specified
          * resolution not being available, etc.
          */
-        fprintf( stderr, "Video mode set failed: %s\n",
-                 SDL_GetError( ) );
-        quit( 1 );
+        fprintf ( stderr, "Video mode set failed: %s\n",
+                  SDL_GetError( ) );
+        quit ( 1 );
     }
 
     /*
      * At this point, we should have a properly setup
      * double-buffered window for use with OpenGL.
      */
-    setup_opengl( width, height );
+    setup_opengl ( width, height );
 
     /*
      * Now we want to begin our normal app process--
